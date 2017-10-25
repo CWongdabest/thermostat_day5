aef89d71ec77ef8fffd4bc4fd08503eb16cbfea8
Lucas Salmins <lucasasalmins@gmail.com>
2017-10-25 17:28:02 +0100

    refactored this.tmeperature

--- jasmine/src/thermostat.js
+++ jasmine/src/thermostat.js
@@ -1,22 +1,22 @@
 function Thermostat() {
-  this._defaultTemp = 20;
+  this._temperature = 20;
   this._maxTemp = 25;
 }
 Thermostat.prototype.temperature = function() {
-  return this._defaultTemp;
+  return this._temperature;
 };
 Thermostat.prototype.up = function(number) {
-  if (this._defaultTemp + number > this._maxTemp) {
+  if (this._temperature + number > this._maxTemp) {
     throw new Error('PSM On: max temp is 25 degrees');
   } else {
-    this._defaultTemp += number;
+    this._temperature += number;
   }
 };
 Thermostat.prototype.down = function(number) {
-  if (this._defaultTemp - number < 10) {
+  if (this._temperature - number < 10) {
     throw new Error('min temp is 10 degrees');
   } else {
-    this._defaultTemp -= number;
+    this._temperature -= number;
   }
 };
 Thermostat.prototype.powerSavingModeOn = function() {
